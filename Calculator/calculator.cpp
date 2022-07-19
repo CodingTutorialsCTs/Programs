9f2e37f870d5397c8958b1b47b693919746cefab
Krish Raj <96698992+Krishraj22@users.noreply.github.com>
2022-07-19 09:42:42 +0530

    Version 19.07.22
    This is an enhanced version where you can use this more efficiently.

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -9,49 +9,51 @@ int main() {
 	while(exits)
 	{
 	    int input;
-	    char int1, int2;
-	    int int3, int4;
-	    cout << "Select Operation:\n";
-	    cout << "1)Addition 2)Subtration 3)Multiplication 4)Division\n";
+	    int int1, int2;
+	    cout << "\nSelect Operation:\n";
+	    cout << "\n1)Addition 2)Subtration 3)Multiplication 4)Division\n";
+		SetConsoleTextAttribute(hConsole, 10);
+		cout << "\n Please do not select any alphabet to add, sub, multiply, and divide, this may gives you ASCII values and may the loop start in some cases:\n";
+		SetConsoleTextAttribute(hConsole, 7);
 	    cin >> input;
 	    switch(input)
 	    {
-	        case 1: cout << "You have selected Addition Operation\n";
-	                cout << "Enter two digit to add\n";
+		    case 1: cout << "ADDITION ::\n";
+	                cout << "Enter numbers to add:\n";
 	                cin >> int1 >> int2;
-	                cout << (int1-48) + (int2-48);
+	                cout << int1 + int2;
 	                cout << "\n";
 	                cout << "Do you want to exit? 0) Yes 1)No:";
 	                cin >> exits;
 	                break;
-	        case 2: cout << "You have selected Subtration Operation\n";
-	                cout << "Enter two digits to sub\n";
-	                cin >> int3 >> int4;
-	                cout << int3 - int4;
+		    case 2: cout << "SUBTRACTION ::\n";
+	                cout << "Enter numbers to sub:\n";
+	                cin >> int1 >> int2;
+	                cout << int1 - int2;
 	                cout << "\n";
 	                cout << "Do you want to exit? 0) Yes 1)No:";
 	                cin >> exits;
 	                break;
-	        case 3: cout << "You have selected Multiplication Operation\n";
-	                cout << "Enter two digits to multiply\n";
-	                cin >> int3 >> int4;
-	                cout << int3 * int4;
+		    case 3: cout << "MULTIPLICATION ::\n";
+	                cout << "Enter numbers to multiply:\n";
+	                cin >> int1 >> int2;
+	                cout << int1 * int2;
 	                cout << "\n";
 	                cout << "Do you want to exit? 0) Yes 1)No:";
 	                cin >> exits;
 	                break;
 
 	       case 4:
-	                cout << "You have selected Division Operation\n";
-	                cout << "Enter two digits to divide\n";
-	                cin >> int3 >> int4;
-	                if (int4 == 0)
+	                cout << "DIVISION ::\n";
+	                cout << "Enter numbers to divide:\n";
+	                cin >> int1 >> int2;
+	                if (int2 == 0)
 	                {
 	                    SetConsoleTextAttribute(hConsole,FOREGROUND_RED);
 	                    cout << "ERROR: You have entered 0 as denominator\n";
 	                    SetConsoleTextAttribute(hConsole, 7);
 	                } else {
-	                    cout << int3/int4;
+	                    cout << int1/int2;
 	                    cout << "\n";
 	                }
 	                cout << "Do you want to exit? 0) Yes 1)No:";
@@ -59,7 +61,7 @@ int main() {
 	                break;
 
 	       default: SetConsoleTextAttribute(hConsole, 14);
-	                cout << "WARNING: you have not selected any valid option please select among 1,2,3,4\n";
+	                cout << "You have not selected any valid option please select among 1,2,3,4\n";
                     SetConsoleTextAttribute(hConsole, 7);
 	                cout << "Do you want to exit? 0) Yes 1)No:";
 	                cin >> exits;
